af88fbfb662bbdbed9978b2e339b709b87a011fc
yihong <yihong@risesoft.net>
2024-11-14 18:08:03 +0800

    档案系统功能完善

---
+++ vue/y9vue-archives/src/api/archives/record.ts
@@ -0,0 +1,29 @@
+import Request from "@/api/lib/request";
+import qs from "qs";
+
+var archivesRequest = new Request();
+//获取门类管理列表
+export function getArchivesRecordList(categoryId,page,rows){
+  const params = {
+    categoryId:categoryId,
+    page:page,
+    rows:rows
+  };
+  return archivesRequest({
+    url: "/vue/record/getArchivesRecordList",
+    method: 'get',
+    params: params
+  });
+}
+
+export function saveFormData(saveType,categoryId,formDataJson){
+    let data = new FormData();
+    data.append("saveType", saveType);
+    data.append("categoryId", categoryId);
+    data.append("formDataJson", formDataJson);
+    return archivesRequest({
+      url: "/vue/record/saveOrUpdate",
+      method: 'post',
+      data: data
+    });
+  }
