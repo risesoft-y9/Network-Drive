91257de2cc1a8e266880c201f32b02d9c64a87af
mengjuhua <mengjuhua@risesoft.net>
2025-01-13 09:53:09 +0800

    chore：init gradle settings

---
+++ risenet-y9boot-webapp-storage/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.y9.docker)
+    alias(libs.plugins.y9.conventions.war)
+    alias(libs.plugins.y9.lombok)
+    alias(libs.plugins.y9.smart.doc)
+    alias(y9libs.plugins.org.springframework.boot)
+}
+
+
+dependencies {
+    api(platform(libs.y9.digitalbase.bom))
+    api(platform(y9libs.spring.boot.bom))
+    providedRuntime(platform(y9libs.spring.boot.bom))
+
+    api("net.risesoft:risenet-y9boot-starter-sso-oauth2-resource")
+    api("net.risesoft:risenet-y9boot-starter-security")
+    api("net.risesoft:risenet-y9boot-starter-jpa-public")
+    api("net.risesoft:risenet-y9boot-starter-jpa-tenant")
+    api("net.risesoft:risenet-y9boot-starter-multi-tenant")
+    api("net.risesoft:risenet-y9boot-api-feignclient-platform")
+    api("net.risesoft:risenet-y9boot-support-file-service-ftp")
+    api("org.springframework.boot:spring-boot-starter-validation")
+    api("net.risesoft:risenet-y9boot-starter-web")
+    api("net.risesoft:risenet-y9boot-common-nacos")
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+}
+
+description = "risenet-y9boot-webapp-storage"
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    // Include 'plugins build' to define convention plugins.
+    //includeBuild("build-logic")
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    //引入y9的版本定义
+    versionCatalogs {
+        create("y9libs") {
+            from("net.risesoft.y9:risenet-gradle-version-catalog:9.7.0-03")
+        }
+    }
+}
+
+rootProject.name = "y9-storage"
+include(":risenet-y9boot-webapp-storage")
+include(":risenet-y9boot-webapp-dataAssets")
+include(":risenet-y9boot-webapp-archives")
