0a908fa091d3d6afb664e63f54bbf2d3c4d9d12e
mengjuhua <mengjuhua@risesoft.net>
2025-03-14 14:35:18 +0800

    fix: 处理参数对不上的问题

--- risenet-y9boot-webapp-storage/src/main/java/net/risesoft/repository/FileNodeCollectRepository.java
+++ risenet-y9boot-webapp-storage/src/main/java/net/risesoft/repository/FileNodeCollectRepository.java
@@ -12,13 +12,13 @@ public interface FileNodeCollectRepository
     extends JpaRepository<FileNodeCollect, String>, JpaSpecificationExecutor<FileNodeCollect> {
 
     @Query("select c.fileId from FileNodeCollect c where c.collectUserId=?1 and c.parentId in(?2)")
-    public List<String> findByCollectUserIdAndParentIdIn(String userId, List<String> listNames);
+    List<String> findByCollectUserIdAndParentIdIn(String userId, List<String> listNames);
 
-    @Query("select c.fileId from FileNodeCollect c where c.parentId=?2 and c.listName =?3 ")
-    public List<String> openCollectFolder(String parentId, String listName);
+    @Query("select c.fileId from FileNodeCollect c where c.parentId=?1 and c.listName =?2 ")
+    List<String> openCollectFolder(String parentId, String listName);
 
     @Query("select c.fileId from FileNodeCollect c where c.collectUserId=?1 ")
-    public List<String> openCollectFolder(String userId);
+    List<String> openCollectFolder(String userId);
 
     FileNodeCollect findByFileIdAndCollectUserIdAndListName(String fileId, String collectUserId, String listName);
 }
