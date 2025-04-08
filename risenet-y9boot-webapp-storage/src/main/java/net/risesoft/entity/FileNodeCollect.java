f656126d9087beeff1ce4850c2acd71763a89542
yihong <yihong@risesoft.net>
2025-04-08 18:39:38 +0800

    增加实体类serialVersionUID

--- risenet-y9boot-webapp-storage/src/main/java/net/risesoft/entity/FileNodeCollect.java
+++ risenet-y9boot-webapp-storage/src/main/java/net/risesoft/entity/FileNodeCollect.java
@@ -26,6 +26,8 @@ import lombok.NoArgsConstructor;
 @Data
 public class FileNodeCollect implements Serializable {
 
+
+    private static final long serialVersionUID = 3258301873386052590L;
     @Id
     @Column(name = "ID", length = 38, nullable = false)
     @GeneratedValue(generator = "uuid")
