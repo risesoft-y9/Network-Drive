ec4af622c88cc0f5a0665bdc07ef10c9c44b189a
mengjuhua <mengjuhua@risesoft.net>
2025-04-27 11:23:22 +0800

    fix:处理javax->jakarta

--- risenet-y9boot-webapp-storage/src/main/java/net/risesoft/entity/FileShareLink.java
+++ risenet-y9boot-webapp-storage/src/main/java/net/risesoft/entity/FileShareLink.java
@@ -3,14 +3,14 @@ package net.risesoft.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.GenericGenerator;
@@ -21,7 +21,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Y9_STORAGE_SHARE_LINK", indexes = {@Index(name = "FILE_SHARE_LINK_FILEID_INDEX", columnList = "FILEID")})
-@org.hibernate.annotations.Table(comment = "文档分享链接表", appliesTo = "Y9_STORAGE_SHARE_LINK")
+@Comment("文档分享链接表")
 @NoArgsConstructor
 @Data
 public class FileShareLink implements Serializable {
