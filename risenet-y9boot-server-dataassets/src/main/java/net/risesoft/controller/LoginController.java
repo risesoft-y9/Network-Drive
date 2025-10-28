7d05d2f24c33349aac910882d6c5e869250f1136
shidaobang <daobangshi@gmail.com>
2025-10-28 14:39:15 +0800

    refactor: 多条件查询使用 Query 对象封装

--- risenet-y9boot-server-dataassets/src/main/java/net/risesoft/controller/LoginController.java
+++ risenet-y9boot-server-dataassets/src/main/java/net/risesoft/controller/LoginController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-import net.risesoft.api.platform.org.dto.CreatePersonDTO;
+import net.risesoft.dto.platform.CreatePersonDTO;
 import net.risesoft.pojo.Y9Result;
+
 import y9.client.rest.platform.org.PersonApiClient;
 
 @RequiredArgsConstructor
@@ -21,27 +22,28 @@ import y9.client.rest.platform.org.PersonApiClient;
 public class LoginController {
 
     private final PersonApiClient personApiClient;
-    
+
     @Value("${y9.app.dataAssets.orgId}")
     private String orgId;
-    
+
     /**
      * 注册账号
+     * 
      * @return
      */
     @PostMapping(value = "/savePerson")
-	public Y9Result<List<String>> savePerson(@RequestHeader("auth-tenantId") String tenantId, @RequestParam String name, 
-			@RequestParam String idCard, @RequestParam String phone, @RequestParam String password) {
-    	CreatePersonDTO personDTO = new CreatePersonDTO();
-    	personDTO.setIdNum(idCard);
-    	personDTO.setIdType("10");
-    	personDTO.setLoginName(name);
-    	personDTO.setMobile(phone);
-    	personDTO.setName(name);
-    	personDTO.setPassword(password);
-    	personDTO.setParentId(orgId);
-		personApiClient.savePersonWithExt(tenantId, personDTO);
-		return Y9Result.successMsg("注册成功");
-	}
+    public Y9Result<List<String>> savePerson(@RequestHeader("auth-tenantId") String tenantId, @RequestParam String name,
+        @RequestParam String idCard, @RequestParam String phone, @RequestParam String password) {
+        CreatePersonDTO personDTO = new CreatePersonDTO();
+        personDTO.setIdNum(idCard);
+        personDTO.setIdType("10");
+        personDTO.setLoginName(name);
+        personDTO.setMobile(phone);
+        personDTO.setName(name);
+        personDTO.setPassword(password);
+        personDTO.setParentId(orgId);
+        personApiClient.savePersonWithExt(tenantId, personDTO);
+        return Y9Result.successMsg("注册成功");
+    }
 
 }
