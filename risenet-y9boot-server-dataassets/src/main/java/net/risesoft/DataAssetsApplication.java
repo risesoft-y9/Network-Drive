cd1a250c97a509af3070759d7473f87e9da2216e
shidaobang <shidaobang@risesoft.net>
2026-03-12 15:54:31 +0800

    feat: 自定义 springboot 应用的 banner，打印一些有用信息

--- risenet-y9boot-server-dataassets/src/main/java/net/risesoft/DataAssetsApplication.java
+++ risenet-y9boot-server-dataassets/src/main/java/net/risesoft/DataAssetsApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import net.risesoft.y9.configuration.Y9Properties;
+import net.risesoft.y9.spring.boot.Y9Banner;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Y9Properties.class)
 public class DataAssetsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DataAssetsApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(DataAssetsApplication.class);
+        springApplication.setBanner(new Y9Banner());
+        springApplication.run(args);
     }
 }
