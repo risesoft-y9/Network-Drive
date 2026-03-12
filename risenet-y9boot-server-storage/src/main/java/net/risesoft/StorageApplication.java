b7f65992b0d717a95259880ae0fb598a9e4f2910
shidaobang <shidaobang@risesoft.net>
2026-03-12 15:54:31 +0800

    feat: 自定义 springboot 应用的 banner，打印一些有用信息
    (cherry picked from commit cd1a250c97a509af3070759d7473f87e9da2216e)

--- risenet-y9boot-server-storage/src/main/java/net/risesoft/StorageApplication.java
+++ risenet-y9boot-server-storage/src/main/java/net/risesoft/StorageApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import net.risesoft.y9.configuration.Y9Properties;
+import net.risesoft.y9.spring.boot.Y9Banner;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Y9Properties.class)
 public class StorageApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StorageApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(StorageApplication.class);
+        springApplication.setBanner(new Y9Banner());
+        springApplication.run(args);
     }
 }
