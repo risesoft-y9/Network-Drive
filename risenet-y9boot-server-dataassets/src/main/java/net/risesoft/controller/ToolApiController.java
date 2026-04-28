4cd034bf60ff10c4b48521eb72206f24c09f19a3
mengjuhua <mengjuhua@risesoft.net>
2026-04-28 16:14:04 +0800

    fix: javax -》 jakarta

--- risenet-y9boot-server-dataassets/src/main/java/net/risesoft/controller/ToolApiController.java
+++ risenet-y9boot-server-dataassets/src/main/java/net/risesoft/controller/ToolApiController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
