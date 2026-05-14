58f795eb1ca7ae2658345a5bb29faf50fe06995b
panzhaoxiong <panzhaoxiong@risesoft.net>
2026-05-14 10:23:41 +0800

    移除dataassets工程
    (cherry picked from commit 698dd37ec3771beaac841449b5effaf965fbddc8)

--- vue/y9vue-dataAssets/src/utils/y9Object.ts
+++
@@ -1,9 +0,0 @@
-export class TableObject {
-    data: any[] = [];
-    currentPage = 1;
-    total = 0;
-    pageSize = 2;
-    pageSizes: number[] = [2, 20, 30, 50];
-    height?: number;
-    multipleSelection: any[] = [];
-}
