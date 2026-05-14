da212692edd2a750d42bd6b1d7fa4b8c33d7115d
yihong <yihong@risesoft.net>
2026-05-14 14:57:30 +0800

    refactor:删除接口
    (cherry picked from commit 133dd7bc9b939bfc1e7c413349dcc3f44c4d1771)

--- y9-module-storage/risenet-y9boot-api-feignclient-storage/src/main/java/y9/client/rest/storage/StorageCapacityApiClient.java
+++ y9-module-storage/risenet-y9boot-api-feignclient-storage/src/main/java/y9/client/rest/storage/StorageCapacityApiClient.java
@@ -1,12 +1,8 @@
 package y9.client.rest.storage;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import net.risesoft.api.storage.StorageCapacityApi;
-import net.risesoft.model.storage.StorageUsageRatioModel;
-import net.risesoft.pojo.Y9Result;
 
 /**
  * 网络硬盘存储容量接口
@@ -18,17 +14,5 @@ import net.risesoft.pojo.Y9Result;
     url = "${y9.service.storage.directUrl:}",
     path = "/${y9.service.storage.name:server-storage}/services/rest/capacity")
 public interface StorageCapacityApiClient extends StorageCapacityApi {
-
-    /**
-     * 获取当前人的文件存储空间使用比例（用于饼状图显示）
-     *
-     * @param tenantId
-     * @param userId
-     * @return
-     */
-    @Override
-    @GetMapping("/getStorageUsageRatio")
-    Y9Result<StorageUsageRatioModel> getStorageUsageRatio(@RequestParam("tenantId") String tenantId,
-        @RequestParam("userId") String userId);
 
 }
