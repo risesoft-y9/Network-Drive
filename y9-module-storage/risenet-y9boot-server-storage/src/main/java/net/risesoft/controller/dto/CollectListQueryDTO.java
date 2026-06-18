9feaf9db44e860ff9b03d96c2d73b9840bc45de8
yihong <yihong@risesoft.net>
2026-06-18 15:29:24 +0800

    refactor:优化代码

--- y9-module-storage/risenet-y9boot-server-storage/src/main/java/net/risesoft/controller/dto/CollectListQueryDTO.java
+++ y9-module-storage/risenet-y9boot-server-storage/src/main/java/net/risesoft/controller/dto/CollectListQueryDTO.java
@@ -2,6 +2,7 @@ package net.risesoft.controller.dto;
 
 import lombok.Data;
 
+import net.risesoft.support.OrderProp;
 import net.risesoft.support.OrderRequest;
 
 /**
@@ -16,5 +17,29 @@ public class CollectListQueryDTO {
 
     private String listType;
 
-    private OrderRequest orderRequest;
+    /**
+     * 排序字段，对应 OrderProp 枚举值，如 FILE_NAME、FILE_SIZE、UPDATE_TIME、CREATE_TIME
+     */
+    private String orderProp;
+
+    /**
+     * 是否升序，默认 true
+     */
+    private Boolean orderAsc;
+
+    /**
+     * 获取排序请求对象，前端参数 orderProp/orderAsc 扁平映射到此方法
+     */
+    public OrderRequest getOrderRequest() {
+        OrderProp prop = OrderProp.FILE_NAME;
+        if (orderProp != null) {
+            try {
+                prop = OrderProp.valueOf(orderProp);
+            } catch (IllegalArgumentException e) {
+                // 无效值时使用默认
+            }
+        }
+        boolean asc = orderAsc == null || orderAsc;
+        return new OrderRequest(prop, asc);
+    }
 }
