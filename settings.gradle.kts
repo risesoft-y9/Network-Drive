51daf68143a22b714db6d899b8e0cbab1d7941b0
mengjuhua <mengjuhua@risesoft.net>
2026-06-23 11:29:06 +0800

    chore: update y9 gradle-version-catalog version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,13 +18,12 @@ dependencyResolutionManagement {
     //引入y9的版本定义
     versionCatalogs {
         create("y9libs") {
-            from("net.risesoft.y9:risenet-gradle-version-catalog:9.7.0-M6")
+            from("net.risesoft.y9:risenet-gradle-version-catalog:9.7.0-RC3")
         }
     }
 }
 
 rootProject.name = "y9-storage"
-include(":risenet-y9boot-server-storage")
-
-include(":risenet-y9boot-api-auth")
-include(":risenet-y9boot-server-dataassets")
+include(":y9-module-storage:risenet-y9boot-api-feignclient-storage")
+include(":y9-module-storage:risenet-y9boot-api-interface-storage")
+include(":y9-module-storage:risenet-y9boot-server-storage")
