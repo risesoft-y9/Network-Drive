24619bf471b5bac9e8a3dbdd51f4e88e17a909cc
yihong <yihong@risesoft.net>
2026-07-09 17:53:12 +0800

    refactor: 标签管理路由增加系统管理员权限

--- vue/y9vue-storage/src/router/modules/tagRouter.js
+++ vue/y9vue-storage/src/router/modules/tagRouter.js
@@ -2,7 +2,7 @@
  * @Author: yihong yihong@risesoft.net
  * @Date: 2025-12-02 17:26:08
  * @LastEditors: yihong yihong@risesoft.net
- * @LastEditTime: 2026-04-03 10:33:36
+ * @LastEditTime: 2026-07-03 17:24:51
  * @FilePath: \y9-vue\y9vue-storage\src\router\modules\tagRouter.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE   
  */
@@ -13,7 +13,7 @@ const tagRouter = {
     name: 'tag',
     meta: {
         title: '标签字典管理',
-        roles: ['tagManager', 'user', 'reportManager', 'capacityManager', 'publicManager']
+        roles: ['tagManager', 'user', 'reportManager', 'capacityManager', 'publicManager', 'systemManager']
     },
     children: [
         {
